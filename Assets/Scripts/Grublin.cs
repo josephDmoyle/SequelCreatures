5aedf1250e995f7e39a87fe508752eda701321ce
josephDmoyle <u0929730@umail.utah.edu>
2020-01-31 21:38:43 -0700

    Hiveminded

---
+++ Assets/Scripts/Grublin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Grublin : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent navMeshAgent = null;
+
+    private void FixedUpdate()
+    {
+        navMeshAgent.SetDestination(Hivemind.hivemind.goal.position);
+    }
+}
---
+++ Assets/Scripts/Hivemind.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Hivemind : MonoBehaviour
+{
+    public static Dictionary<Team, Hivemind> hiveminds = new Dictionary<Team, Hivemind>();
+
+
+    [SerializeField] Team team;
+
+    UnityEvent<Transform> pursueEvent;
+    Transform goal = null;
+
+    private void Awake()
+    {
+        hiveminds[team] = this;
+    }
+
+    private void FixedUpdate()
+    {
+        if(goal && pursueEvent != null)
+            pursueEvent.Invoke(goal);
+    }
+}
+
+public enum Team
+{
+    Protags,
+    Antags,
+}
