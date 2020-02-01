3baa0ba774a5ce219e544fb5054d2e3c0062bf71
Ethan Bowen <Ethankbowen@gmail.com>
2020-01-31 22:36:18 -0700

    Character selecting works

---
+++ Assets/Scripts/Midwife.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Midwife : Controllable
+{
+    [SerializeField] CharacterController characterController = null;
+    public override void Control()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        characterController.Move(transform.right * horizontal);
+        characterController.Move(transform.forward * vertical);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Controllable player;
+    [SerializeField] private List<GameObject> cameras;
+    [SerializeField] private List<GameObject> characters;
+    //[SerializeField] GameObject JunkyardCam;
+    //[SerializeField] GameObject GreenRoomCam;
+
+    int currentCharacter;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,5 +25,24 @@ public class PlayerController : MonoBehaviour
         {
             player.Control();
         }
+
+        if(Input.GetButtonDown("Camera"))
+        {
+            SelectCharacter(currentCharacter + 1);
+        }
+    }
+
+    void SelectCharacter(int newCharacter)
+    {
+        if(newCharacter > 3)
+        {
+            newCharacter = 0;
+        }
+        cameras[newCharacter].SetActive(true);
+
+
+        cameras[currentCharacter].SetActive(false);
+        player = characters[newCharacter].GetComponent<Controllable>();
+        currentCharacter = newCharacter;
     }
 }
