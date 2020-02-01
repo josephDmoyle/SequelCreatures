49ef460b0df57f61115d2c84f4552746bfc592bd
josephDmoyle <u0929730@umail.utah.edu>
2020-01-31 20:25:42 -0700

    Collision Layers and Scavenger Added

--- Assets/Scripts/Scavenger.cs
+++ Assets/Scripts/Scavenger.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Scavenger : Controllable
 {
+    [SerializeField] CharacterController characterController = null;
     public override void Control()
     {
+
     }
 }
