91415f32e30339443acd26fba3854c433c8d13d0
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2020-02-01 11:57:11 -0700

    Working on Spawn tester

---
+++ Assets/Scripts/DieScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieScript : MonoBehaviour
+{
+
+    public Vector3 x;
+    float DieY;
+    float DieZ;
+    public float speed;
+    public GameObject PitFall;
+    public float targetX;
+
+    // Start is called before the first frame update
+    public void Die()
+    {
+        print("Success");
+        float step = speed * Time.deltaTime;
+        while (transform.position.x < targetX)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PitFall.transform.position, step);
+        }
+       
+    }
+}
