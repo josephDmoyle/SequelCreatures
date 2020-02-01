7bfba3dbf04bee0f9dbefe963e398813e8691d1b
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2020-02-01 12:55:13 -0700

    Spy Level

---
+++ Assets/Scripts/Spy_Scripts/FollowThePath.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowThePath : MonoBehaviour
+{
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    // Use this for initialization
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+        // Set position of Enemy as position of the first waypoint
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    { 
+        // Move Enemy
+        Move();
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector3.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+        if (waypointIndex > waypoints.Length - 1)
+        {
+            waypointIndex = 0;
+        }
+
+    }
+}
