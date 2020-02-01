99ac2f2d70bba8933757ab648090016b3e6c3953
Ethan Bowen <Ethankbowen@gmail.com>
2020-02-01 14:42:42 -0700

    Traps will hurt the scavenger until it dies

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> junk = new List<GameObject>();
+    [SerializeField] private List<GameObject> traps = new List<GameObject>();
 
     private int dumpTimer = 0;
     [SerializeField] int dumpTime = 1000;
@@ -36,6 +37,13 @@ public class GameController : MonoBehaviour
             int junkIndex = (int)(Random.Range(0.0f, 0.99999999f) * (float)junk.Count);
 
             Instantiate(junk[junkIndex], new Vector3(Random.Range(-150, -51), 0, Random.Range(-50, 50)), Quaternion.identity);
+        }
+
+        for (int i = 0; i < junkAmount/2; i++)
+        {
+            int trapIndex = (int)(Random.Range(0.0f, 0.99999999f) * (float)traps.Count);
+
+            Instantiate(traps[trapIndex], new Vector3(Random.Range(-150, -51), 0, Random.Range(-50, 50)), Quaternion.identity);
         }
     }
 }
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Health.healths[collision.transform.root.gameObject].Damage(1);
+    }
+}
