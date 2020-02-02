ba07dcac7ba8cc69458db32b1b315ad997052ee5
Hayden Matthews <haydenamatthews@yahoo.com>
2020-02-02 10:42:10 -0700

    Merged spy into main scene

--- Assets/Scripts/Spy_Scripts/SpyController.cs
+++ Assets/Scripts/Spy_Scripts/SpyController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpyController : MonoBehaviour
+public class SpyController : Controllable
 {
     [SerializeField] SpriteBillboard sb;
+    [SerializeField] CharacterController characterController = null;
     Rigidbody rb;
     public float speed = 10.0f;
     private GameObject player;
+    private Vector3 raycastDirect = Vector3.right;
     public bool defeated = false;
     public Animator animator;
 
@@ -21,32 +23,7 @@ public class SpyController : MonoBehaviour
     }
     void Update()
     {
-        if (!defeated)
-        {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rb.velocity = transform.right * speed;
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rb.velocity = -transform.right * speed;
-            }
-            else if (Input.GetKey(KeyCode.UpArrow))
-            {
-                rb.velocity = new Vector3(0, 0, 1) * speed;
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                rb.velocity = new Vector3(0, 0, -1) * speed;
-            }
-            else
-            {
-                rb.velocity = new Vector3();
-            }
-            animator.SetBool("isIdle", rb.velocity.magnitude <= 0);
-            animator.SetBool("isWalking", rb.velocity.magnitude > 0);
-
-        }
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -81,6 +58,47 @@ public class SpyController : MonoBehaviour
                 gameObject.tag = "TakeOver";
                 sb.gameObject.SetActive(false);
             }
+        }
+    }
+
+    public override void Control()
+    {
+        if (!defeated)
+        {
+            //if (Input.GetKey(KeyCode.RightArrow))
+            //{
+            //    rb.velocity = transform.right * speed;
+            //}
+            //else if (Input.GetKey(KeyCode.LeftArrow))
+            //{
+            //    rb.velocity = -transform.right * speed;
+            //}
+            //else if (Input.GetKey(KeyCode.UpArrow))
+            //{
+            //    rb.velocity = new Vector3(0, 0, 1) * speed;
+            //}
+            //else if (Input.GetKey(KeyCode.DownArrow))
+            //{
+            //    rb.velocity = new Vector3(0, 0, -1) * speed;
+            //}
+            //else
+            //{
+            //    rb.velocity = new Vector3();
+            //}
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            if (horizontal != 0 || vertical != 0)
+            {
+                raycastDirect = (transform.position + (new Vector3(horizontal, 0, vertical)).normalized);
+            }
+
+            transform.LookAt(raycastDirect);
+
+            characterController.Move(transform.forward * Time.fixedDeltaTime * speed * Mathf.Min(1.0f, (Mathf.Abs(horizontal) + Mathf.Abs(vertical))));
+            animator.SetBool("isIdle", rb.velocity.magnitude <= 0);
+            animator.SetBool("isWalking", rb.velocity.magnitude > 0);
+
         }
     }
 }
