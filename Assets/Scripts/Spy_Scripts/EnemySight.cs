1ee87f61054b0d316e0d6037dd504c24752c5a15
S4C-Olympus <54639306+S4C-Olympus@users.noreply.github.com>
2020-02-01 17:46:06 -0700

    agjh

--- Assets/Scripts/Spy_Scripts/EnemySight.cs
+++ Assets/Scripts/Spy_Scripts/EnemySight.cs
@@ -7,57 +7,38 @@ public class EnemySight : MonoBehaviour
 {
     public float fieldOfViewAngle = 110f;
     public bool playerInSight;
-    public Vector3 personalLastSighting;
 
-    private NavMeshAgent nav;
     private SphereCollider col;
-   // private LastPlayerSighting lastPlayerSighting;
     private GameObject player;
-    private Vector3 previousSighting;
 
     void Awake()
     {
-        nav = GetComponent<NavMeshAgent>();
         col = GetComponent<SphereCollider>();
-      //  lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<lastPlayerSighting>();
         player = GameObject.FindGameObjectWithTag("Player");
-
-       // personalLastSighting = lastPlayerSighting.reserPositon;
-       // previousSighting = lastPlayerSighting.resetPosition;
     }
 
     void Update()
     {
-       // if(lastPlayerSighting.position != previousSighting)
-       // {
-        //    personalLastSighting = lastPlayerSighting.positon;
-       // }
-
-       // previousSighting = lastPlayerSighting.position;
+      
     }
 
     void OnTriggerStay(Collider other)
     {
-        if(other.gameObject == player)
+        //if tag eplayer collides with sphere colider or enemy collider
+        if (other.gameObject.tag == "Player")
         {
-            playerInSight = false;
-
-            Vector3 direction = other.transform.position - transform.position;
-            float angle = Vector3.Angle(direction, transform.forward);
-
-            if(angle < fieldOfViewAngle * 0.5f)
-            {
-                RaycastHit hit;
-
-                if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
-                {
-                    if(hit.collider .gameObject == player)
-                    {
-                        playerInSight = true;
-                        //lastPlayerSighting.position = playter.transform.position;
-                    }
-                }
-            }
+            //Destory player
+            Destroy(player);
         }
     }
+
+
+        void OnCollisionEnter(Collision other)
+        {
+
+            if (other.gameObject.tag == "Player")
+            {
+                Destroy(player);
+            }
+        }
 }
